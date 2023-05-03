1e271e3b924aae043f9b81bf37d4538802724d31
Gabriel Sanchez <gabriel@lendaas.com>
2023-05-02 20:20:44 -0400

    Imp/added form tags and types

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,31 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [inputValue, setInputValue] = useState('');
-  const [chatLog, setChatLog] = useState([]);
+  const [chatLog, setChatLog] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-  }
 
+    setChatLog((prevChatLog:any) => [...prevChatLog, {type: 'user', message: inputValue}]);
+
+    setInputValue('');
+  }
+  interface Message {
+    message: String
+  }
   return (
     <>
     <h1>Smart Chat</h1>
-    <input type="text" placeholder="Type your here..." value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-    <button type="submit">Send message</button>
+    {
+      chatLog.map((message:Message, index:string) => (
+        <div key={index}>{message.message}</div>
+      ))
+    }
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Type your here..." value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+      <button type="submit">Send message</button>
+    </form>
     </>
   )
 }
